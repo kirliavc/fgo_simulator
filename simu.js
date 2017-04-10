dad6e63532f1782a4248d1b7448a8a472dfa941f
kirliavc <jlc@pku.edu.cn>
2017-04-11 02:37:43 +0800

    Add files via upload

---
+++ simu.js
@@ -0,0 +1,118 @@
+var log=new Array(11);
+//0:4serv,1:3serv,2:4craft,3:3craft
+var serv5=new Array("002","008","037","052","059","060","062","065","075","076","097");
+var craft5=new Array("031","032","033","034","035","048","057","058","067","075","097","175");
+var serv4=new Array("006","010","011","014","018","029","030","041","046","047","048","058","066","074","082","087","089","094","100","101");
+var serv3=new Array("007","009","013","015","017","020","022","023","026","027","028","031","035","042","049","055","056","063","064","071","072","079","080","081","095","104","105");
+var craft4=new Array("021","022","023","024","025","026","027","028","029","030","038","039","047","056","066","073","074","098","176");
+var upcraft4=new Array("182","183","184");
+var craft3=new Array("037","042","046","055","065","072","089","090","091","092","093","094","095","096");
+
+function getOne(i,j){
+    var rand;
+    if(j==0)
+        rand=Math.random();
+    else
+        rand=j;
+    if(rand<0.01){ //5,servant
+        log[i]=0;
+        if(rand<0.0065){    //up
+            $("#r_"+i).attr("src","http://file.fgowiki.591mogu.com/fgo/head/106.jpg");
+            return;
+        }
+        var bias=(0.01-0.0065)/serv5.length;
+        for(var r=0;r<serv5.length;r=r+1){  //not up
+            if(rand>=0.0065+r*bias&&rand<0.0065+(r+1)*bias){
+                $("#r_"+i).attr("src","http://file.fgowiki.591mogu.com/fgo/head/"+serv5[r]+".jpg");
+                
+                return;
+            }
+        }
+    }
+    if(rand<0.05){  //5,craft
+        log[i]=2;
+        var bias=(0.05-0.01)/craft5.length;
+        for(var r=0;r<craft5.length;r=r+1){
+            if(rand>=0.01+r*bias&&rand<0.01+(r+1)*bias){
+                $("#r_"+i).attr("src","http://fgowiki.com/fgo/equip/"+craft5[r]+".jpg");
+                
+                return;
+            }
+        }
+    }
+    if(rand<0.08){  //4,servant
+        log[i]=0;
+        var bias=(0.08-0.05)/serv4.length;
+        for(var r=0;r<serv4.length;r=r+1){
+            if(rand>=0.05+r*bias&&rand<0.05+(r+1)*bias){
+                $("#r_"+i).attr("src","http://file.fgowiki.591mogu.com/fgo/head/"+serv4[r]+".jpg");
+                
+                return;
+            }
+        }
+    }
+    if(rand<0.2){   //4,craft
+        log[i]=2;
+        var bias=0.024;
+        for(var r=0;r<upcraft4.length;r=r+1){   //up
+            if(rand>=0.08+r*bias&&rand<0.08+(r+1)*bias){
+                $("#r_"+i).attr("src","http://fgowiki.com/fgo/equip/"+upcraft4[r]+".jpg");
+                return;
+            }
+        }
+        bias=(0.2-0.152)/craft4.length;
+        for(var r=0;r<craft4.length;r=r+1){     //not up
+            if(rand>=0.152+r*bias&&rand<0.152+(r+1)*bias){
+                $("#r_"+i).attr("src","http://fgowiki.com/fgo/equip/"+craft4[r]+".jpg");
+                return;
+            }
+        }
+    }
+    if(rand<0.6){   //3,servant
+        log[i]=1;
+        var bias=(0.6-0.2)/serv3.length;
+        for(var r=0;r<serv3.length;r=r+1){
+            if(rand>=0.2+r*bias&&rand<0.2+(r+1)*bias){
+                $("#r_"+i).attr("src","http://file.fgowiki.591mogu.com/fgo/head/"+serv3[r]+".jpg");
+                
+                return;
+            }
+        }
+    }
+    //3,craft
+    var bias=(1-0.6)/craft3.length;
+    for(var r=0;r<craft3.length;r=r+1){     //not up
+        if(rand>=0.6+r*bias&&rand<0.6+(r+1)*bias){
+            $("#r_"+i).attr("src","http://fgowiki.com/fgo/equip/"+craft3[r]+".jpg");
+            return;
+        }
+    }
+}
+function getTen(){
+    log=new Array(11);
+    for(var i=1;i<=10;i++)
+        getOne(i,0);
+    var pro=1,mini=5;
+    for(var i=1;i<=10;i++){
+        if(log[i]%2==0)
+            pro=2;
+        mini=Math.min(mini,log[i]);
+    }
+    console.log(pro);
+    if(pro==1){
+        var c=parseInt(Math.random()*10+1);
+        getOne(c,Math.random()*0.2);
+    }
+    if(mini>=2){
+        var c=parseInt(Math.random()*10+1);
+        var cnt=0;
+        while(log[c]%2==0&&cnt<10){
+            c=parseInt(Math.random()*10+1);
+            cnt+=1;
+        }
+        var rand=Math.random()*0.44;
+        if(rand>0.01)rand=rand+0.04;
+        if(rand>0.04)rand+=0.12;
+        getOne(c,rand);
+    }
+}
