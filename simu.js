5296653d020215aba7319dec7319616325eb6a12
kirliavc <jlc@pku.edu.cn>
2017-04-11 16:48:20 +0800

    summon log

--- simu.js
+++ simu.js
@@ -8,10 +8,10 @@ var serv4=new Array("006","010","011","014","018","029","030","041","046","047",
 var serv3=new Array("007","009","013","015","017","020","022","023","026","027","028","031","035","042","049","055","056","063","064","071","072","079","080","081","095","104","105");
 var craft4=new Array("021","022","023","024","025","026","027","028","029","030","038","039","047","056","066","073","074","098","176");
 var upcraft4=new Array("182","183","184");
-var craft3=new Array("037","042","046","055","065","072","089","090","091","092","093","094","095","096","177");
+var craft3=new Array("037","042","046","055","065","072","089","090","091","092","093","094","095","096");
 
 function getOne(i,j){
-    
+    var imgurl;
     var rand;
     if(j==0){
         times=times+1;
@@ -25,14 +25,17 @@ function getOne(i,j){
         log[i]=0;
         if(rand<0.0065){    //up
             alter++;
-            $("#r_"+i).attr("src","http://file.fgowiki.591mogu.com/fgo/head/106.jpg");
+            imgurl="http://file.fgowiki.591mogu.com/fgo/head/106.jpg";
+            $("#r_"+i).attr("src",imgurl);
+            $("#serv5").append("<img class=\"img-thumbnail\" src=\""+imgurl+"\"></img> ");
             return;
         }
         var bias=(0.01-0.0065)/serv5.length;
         for(var r=0;r<serv5.length;r=r+1){  //not up
             if(rand>=0.0065+r*bias&&rand<0.0065+(r+1)*bias){
-                $("#r_"+i).attr("src","http://file.fgowiki.591mogu.com/fgo/head/"+serv5[r]+".jpg");
-                
+                imgurl="http://file.fgowiki.591mogu.com/fgo/head/"+serv5[r]+".jpg";
+                $("#r_"+i).attr("src",imgurl);
+                $("#serv5").append("<img class=\"img-thumbnail\" src=\""+imgurl+"\"></img> ");
                 return;
             }
         }
@@ -42,8 +45,9 @@ function getOne(i,j){
         var bias=(0.05-0.01)/craft5.length;
         for(var r=0;r<craft5.length;r=r+1){
             if(rand>=0.01+r*bias&&rand<0.01+(r+1)*bias){
-                $("#r_"+i).attr("src","http://fgowiki.com/fgo/equip/"+craft5[r]+".jpg");
-                
+                imgurl="http://fgowiki.com/fgo/equip/"+craft5[r]+".jpg";
+                $("#r_"+i).attr("src",imgurl);
+                $("#craft5").append("<img class=\"img-thumbnail\" src=\""+imgurl+"\"></img> ");
                 return;
             }
         }
@@ -53,8 +57,9 @@ function getOne(i,j){
         var bias=(0.08-0.05)/serv4.length;
         for(var r=0;r<serv4.length;r=r+1){
             if(rand>=0.05+r*bias&&rand<0.05+(r+1)*bias){
-                $("#r_"+i).attr("src","http://file.fgowiki.591mogu.com/fgo/head/"+serv4[r]+".jpg");
-                
+                imgurl="http://file.fgowiki.591mogu.com/fgo/head/"+serv4[r]+".jpg";
+                $("#r_"+i).attr("src",imgurl);
+                $("#serv4").append("<img class=\"img-thumbnail\" src=\""+imgurl+"\"></img> ");
                 return;
             }
         }
@@ -64,14 +69,18 @@ function getOne(i,j){
         var bias=0.024;
         for(var r=0;r<upcraft4.length;r=r+1){   //up
             if(rand>=0.08+r*bias&&rand<0.08+(r+1)*bias){
-                $("#r_"+i).attr("src","http://fgowiki.com/fgo/equip/"+upcraft4[r]+".jpg");
+                imgurl="http://fgowiki.com/fgo/equip/"+upcraft4[r]+".jpg";
+                $("#r_"+i).attr("src",imgurl);
+                $("#craft4").append("<img class=\"img-thumbnail\" src=\""+imgurl+"\"></img> ");
                 return;
             }
         }
         bias=(0.2-0.152)/craft4.length;
         for(var r=0;r<craft4.length;r=r+1){     //not up
             if(rand>=0.152+r*bias&&rand<0.152+(r+1)*bias){
-                $("#r_"+i).attr("src","http://fgowiki.com/fgo/equip/"+craft4[r]+".jpg");
+                imgurl="http://fgowiki.com/fgo/equip/"+craft4[r]+".jpg";
+                $("#r_"+i).attr("src",imgurl);
+                $("#craft4").append("<img class=\"img-thumbnail\" src=\""+imgurl+"\"></img> ");
                 return;
             }
         }
@@ -81,7 +90,8 @@ function getOne(i,j){
         var bias=(0.6-0.2)/serv3.length;
         for(var r=0;r<serv3.length;r=r+1){
             if(rand>=0.2+r*bias&&rand<0.2+(r+1)*bias){
-                $("#r_"+i).attr("src","http://file.fgowiki.591mogu.com/fgo/head/"+serv3[r]+".jpg");
+                imgurl="http://file.fgowiki.591mogu.com/fgo/head/"+serv3[r]+".jpg";
+                $("#r_"+i).attr("src",imgurl);
                 
                 return;
             }
@@ -91,7 +101,8 @@ function getOne(i,j){
     var bias=(1-0.6)/craft3.length;
     for(var r=0;r<craft3.length;r=r+1){     //not up
         if(rand>=0.6+r*bias&&rand<0.6+(r+1)*bias){
-            $("#r_"+i).attr("src","http://fgowiki.com/fgo/equip/"+craft3[r]+".jpg");
+            imgurl="http://fgowiki.com/fgo/equip/"+craft3[r]+".jpg";
+            $("#r_"+i).attr("src",imgurl);
             return;
         }
     }
@@ -100,20 +111,20 @@ function getTen(){
     log=new Array(11);
     for(var i=1;i<=10;i++)
         getOne(i,0);
-    var pro=1,mini=5;
+    var pro=1,mini=5;   //pro: golden card, mini:servant
     for(var i=1;i<=10;i++){
         if(log[i]%2==0)
             pro=2;
         mini=Math.min(mini,log[i]);
     }
-    if(pro==1){
+    if(pro==1){     //no golden card,randomly pick a silver card
         var c=parseInt(Math.random()*10+1);
         getOne(c,Math.random()*0.2);
     }
     if(mini>=2){
         var c=parseInt(Math.random()*10+1);
         var cnt=0;
-        while(log[c]%2==0&&cnt<10){
+        while(log[c]%2==0&&cnt<10){     //no servant, randomly pick a silver card
             c=parseInt(Math.random()*10+1);
             cnt+=1;
         }
